ff566d87a70223e3b48127d9259b1108cf6eb017
seonwoo_jung <laborlawseon@gmail.com>
2024-01-27 19:28:50 +0900

    init

---
+++ src/test/java/com/tobe/healthy/HealthyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobe.healthy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
