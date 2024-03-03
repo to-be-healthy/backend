5a817b1b2c3839aa23e9479f4a320ed91d216f38
jungseonw00 <laborlawseon@gmail.com>
2024-03-03 15:21:34 +0900

    .

--- src/main/java/com/tobe/healthy/HealthyApplication.java
+++ src/main/java/com/tobe/healthy/HealthyApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 
 public class HealthyApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(HealthyApplication.class, args);
     }
 }
