05cd9d2838a04b2cf114d52143c2be8bf28cae27
jungseonw00 <laborlawseon@gmail.com>
2024-03-03 15:22:26 +0900

    fix: SpringSecurity exclude 설정 삭제

--- src/main/java/com/tobe/healthy/HealthyApplication.java
+++ src/main/java/com/tobe/healthy/HealthyApplication.java
@@ -2,13 +2,10 @@ package com.tobe.healthy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-
+@SpringBootApplication
 public class HealthyApplication {
     public static void main(String[] args) {
-
         SpringApplication.run(HealthyApplication.class, args);
     }
 }
