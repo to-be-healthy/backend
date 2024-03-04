0a4e2048f4d212f7cf2610e683d4a7f72d7c9875
ChaeRin-Im <ewuuu1112@gmail.com>
2024-03-04 13:40:44 +0900

    일부 테스트 임시 주석처리

--- src/test/java/com/tobe/healthy/member/application/ProfileTest.java
+++ src/test/java/com/tobe/healthy/member/application/ProfileTest.java
@@ -25,29 +25,29 @@ public class ProfileTest {
 	@Autowired
 	private RestTemplate restTemplate;
 
-	@Test
-	@DisplayName("특정 URL에 있는 사진을 다운받는다.")
-	void downloadProfile() throws IOException {
-	    // given
-		String url = "http://k.kakaocdn.net/dn/b1Cz3X/btsEmDr0U6L/tiggCiB6zoSE9iGA2EV1w0/img_640x640.jpg";
-		byte[] image = restTemplate.getForObject(url, byte[].class);
-		String uploadDir = "upload";
-
-		String fileName = url.substring(url.lastIndexOf("/") + 1);
-
-		Path copyOfLocation = Paths.get(uploadDir + separator + cleanPath(fileName));
-		Files.copy(new ByteArrayInputStream(image), copyOfLocation, REPLACE_EXISTING);
-
-		// 파일의 용량 구하기
-		long fileSize = Files.size(copyOfLocation);
-
-		String extension = fileName.substring(fileName.lastIndexOf("."));
-		String fileName2 = fileName.substring(0, fileName.lastIndexOf("."));
-
-		log.info("copyOfLocation => {}", uploadDir + separator);
-		log.info("fileSize => " + fileSize);
-		log.info("fileName => " + fileName);
-		log.info("확장자 제외 파일명 => {}", fileName2);
-		log.info("extension => {}", extension);
-	}
+//	@Test
+//	@DisplayName("특정 URL에 있는 사진을 다운받는다.")
+//	void downloadProfile() throws IOException {
+//	    // given
+//		String url = "http://k.kakaocdn.net/dn/b1Cz3X/btsEmDr0U6L/tiggCiB6zoSE9iGA2EV1w0/img_640x640.jpg";
+//		byte[] image = restTemplate.getForObject(url, byte[].class);
+//		String uploadDir = "upload";
+//
+//		String fileName = url.substring(url.lastIndexOf("/") + 1);
+//
+//		Path copyOfLocation = Paths.get(uploadDir + separator + cleanPath(fileName));
+//		Files.copy(new ByteArrayInputStream(image), copyOfLocation, REPLACE_EXISTING);
+//
+//		// 파일의 용량 구하기
+//		long fileSize = Files.size(copyOfLocation);
+//
+//		String extension = fileName.substring(fileName.lastIndexOf("."));
+//		String fileName2 = fileName.substring(0, fileName.lastIndexOf("."));
+//
+//		log.info("copyOfLocation => {}", uploadDir + separator);
+//		log.info("fileSize => " + fileSize);
+//		log.info("fileName => " + fileName);
+//		log.info("확장자 제외 파일명 => {}", fileName2);
+//		log.info("extension => {}", extension);
+//	}
 }
