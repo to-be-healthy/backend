05e37276707112cf8e7d62702de70559653c4f46
jungseonw00 <laborlawseon@gmail.com>
2024-03-05 22:22:56 +0900

    Fix: "Schedule DTO 수정 및 Swagger 추가"

--- src/main/java/com/tobe/healthy/schedule/domain/dto/in/AutoCreateScheduleCommandRequest.java
+++ src/main/java/com/tobe/healthy/schedule/domain/dto/in/AutoCreateScheduleCommandRequest.java
@@ -1,5 +1,6 @@
 package com.tobe.healthy.schedule.domain.dto.in;
 
+import jakarta.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,9 +12,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class AutoCreateScheduleCommandRequest {
+	@NotEmpty(message = "트레이너 아이디를 입력해 주세요.")
 	private Long trainer;
+
+	@NotEmpty(message = "수업 시작 시간을 입력해 주세요.")
 	private LocalDateTime startDt;
+
+	@NotEmpty(message = "수업 종료 시간을 입력해 주세요.")
 	private LocalDateTime endDt;
+
+	@NotEmpty(message = "세션당 수업 시간을 입력해 주세요.")
 	private int lessonTime;
+
+	@NotEmpty(message = "세션간 휴식 시간을 입력해 주세요.")
 	private int breakTime;
 }
--- src/main/java/com/tobe/healthy/schedule/presentation/ScheduleController.java
+++ src/main/java/com/tobe/healthy/schedule/presentation/ScheduleController.java
@@ -5,6 +5,10 @@ import com.tobe.healthy.schedule.application.ScheduleService;
 import com.tobe.healthy.schedule.domain.dto.in.AutoCreateScheduleCommandRequest;
 import com.tobe.healthy.schedule.domain.dto.in.ScheduleCommandRequest;
 import com.tobe.healthy.schedule.domain.dto.out.ScheduleCommandResult;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,12 +25,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/schedule")
 @Slf4j
+@Tag(name = "schedule", description = "일정 API")
 public class ScheduleController {
 
 	private final ScheduleService scheduleService;
 
+	@Operation(summary = "수업시작시간, 종료시간, 수업시간, 휴식시간을 설정하면 임시 일정을 생성한다.",
+		responses = {
+			@ApiResponse(responseCode = "200", description = "임시 일정 생성 완료")
+	})
 	@PostMapping("/auto-create")
-	public ResponseEntity<List<ScheduleCommandResponse>> autoCreateSchedule(@RequestBody AutoCreateScheduleCommandRequest request) {
+	public ResponseEntity<List<ScheduleCommandResponse>> autoCreateSchedule(@RequestBody @Valid AutoCreateScheduleCommandRequest request) {
 		return ResponseEntity.ok(scheduleService.autoCreateSchedule(request));
 	}
 
