e5a26cd4079b67f851892c21f12e40e635e88762
jungseonw00 <laborlawseon@gmail.com>
2024-03-11 21:33:39 +0900

    fix: "aws에 배포시 swagger cors 에러 문제 해결"

--- src/main/java/com/tobe/healthy/config/SwaggerConfig.java
+++ src/main/java/com/tobe/healthy/config/SwaggerConfig.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -31,10 +32,12 @@ public class SwaggerConfig {
 				.name(jwtSchemeName)
 				.type(HTTP)
 				.scheme("bearer")
-				.bearerFormat("JWT"));
+				.bearerFormat("JWT"))
+			.addSecuritySchemes("basicScheme", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic"));
 
 		return new OpenAPI()
 			.info(info)
+			.addServersItem(new Server().url("/"))
 			.addSecurityItem(securityRequirement)
 			.components(components);
 	}
