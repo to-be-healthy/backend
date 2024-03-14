2f5b1f95dc2ab487ac0089aa6bcfac28d4f3ddf8
seonwoo_jung <laborlawseon@gmail.com>
2024-03-14 18:26:45 +0900

    .

--- src/main/java/com/tobe/healthy/HealthyApplication.java
+++ src/main/java/com/tobe/healthy/HealthyApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HealthyApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(HealthyApplication.class, args);
     }
 }
