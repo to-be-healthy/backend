8b828e98efb93049c72a7c5302244d7df261f95c
seonwoo_jung <laborlawseon@gmail.com>
2024-03-20 04:33:09 +0900

    fix: "change swagger setting and security config "

--- src/main/java/com/tobe/healthy/config/SwaggerConfig.java
+++ src/main/java/com/tobe/healthy/config/SwaggerConfig.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -36,7 +35,6 @@ public class SwaggerConfig {
 
 		return new OpenAPI()
 			.info(info)
-			.addServersItem(new Server().url("https://api.to-be-healthy.site"))
 			.addSecurityItem(securityRequirement)
 			.components(components);
 	}
