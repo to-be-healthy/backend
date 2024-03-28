ba99b9d0f2ddc8ff523a7ef7b086053516a2117f
ChaeRin-Im <ewuuu1112@gmail.com>
2024-03-28 10:39:46 +0900

    Fix: 초대가입 링크 url 수정

--- src/main/java/com/tobe/healthy/trainer/application/TrainerService.java
+++ src/main/java/com/tobe/healthy/trainer/application/TrainerService.java
@@ -57,9 +57,8 @@ public class TrainerService {
         for(String email : emails){
             memberRepository.findByEmail(email).ifPresent(i -> {throw new CustomException(ErrorCode.MEMBER_ALREADY_MAPPED);});
             String uuid = System.currentTimeMillis() + "_" + UUID.randomUUID();
-            //TODO: 프론트와 상의 후 초대링크 페이지 url 수정하기
             String invitationKey = RedisKeyPrefix.INVITATION.getDescription() + uuid;
-            String invitationLink = "http://www.temp.com?" + uuid;
+            String invitationLink = "https://www.to-be-healthy.site?" + uuid;
             sendInviteLink(email, trainer, invitationLink);
             Map<String, String> invitedMapping = new HashMap<>() {{
                 put("trainerId", trainer.getId().toString());
