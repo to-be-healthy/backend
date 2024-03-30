5433b74b85cf98f507efd7d59582aaeb6a98de20
seonwoo_jung <laborlawseon@gmail.com>
2024-03-31 00:21:26 +0900

    fix: "swagger 설명 추가"

--- src/main/java/com/tobe/healthy/member/domain/dto/in/SocialLoginCommand.java
+++ src/main/java/com/tobe/healthy/member/domain/dto/in/SocialLoginCommand.java
@@ -11,15 +11,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Schema(description = "소셜 로그인 DTO")
 public class SocialLoginCommand {
-	@Schema(description = "인가코드")
+	@Schema(description = "인가코드(카카오, 네이버, 구글 소셜 로그인시 필요)")
     private String code;
 
-	@Schema(description = "상태코드(CSRF 방지)")
+	@Schema(description = "상태코드(CSRF 방지), 네이버 소셜 로그인시 필요")
 	private String state;
 
-	@Schema(description = "회원구분")
+	@Schema(description = "회원구분(모든 소셜 로그인시 필요)")
 	private MemberType memberType;
 
-	@Schema(description = "redirect URL")
+	@Schema(description = "Redirect URL(카카오 소셜 로그인시 필요)")
 	private String redirectUrl;
 }
--- src/main/java/com/tobe/healthy/member/presentation/MemberAuthController.java
+++ src/main/java/com/tobe/healthy/member/presentation/MemberAuthController.java
@@ -189,7 +189,7 @@ public class MemberAuthController {
 			@ApiResponse(responseCode = "200", description = "요청 처리에 성공하였습니다.")
 	})
 	@PostMapping("/access-token/naver")
-	public ResponseHandler<Tokens> getNaverAccessToken(@RequestBody SocialLoginCommand request) {
+	public ResponseHandler<Tokens> getNaverAccessToken(@ParameterObject @RequestBody SocialLoginCommand request) {
 		return ResponseHandler.<Tokens>builder()
 			.data(memberService.getNaverAccessToken(request))
 			.message("요청이 처리되었습니다.")
@@ -205,7 +205,7 @@ public class MemberAuthController {
 			@ApiResponse(responseCode = "200", description = "요청 처리에 성공하였습니다.")
 	})
 	@PostMapping("/access-token/kakao")
-	public ResponseHandler<Tokens> getKakaoAccessToken(@RequestBody SocialLoginCommand request) {
+	public ResponseHandler<Tokens> getKakaoAccessToken(@ParameterObject @RequestBody SocialLoginCommand request) {
 		return ResponseHandler.<Tokens>builder()
 			.data(memberService.getKakaoAccessToken(request))
 			.message("요청이 처리되었습니다.")
