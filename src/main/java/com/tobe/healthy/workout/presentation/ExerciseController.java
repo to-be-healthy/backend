9a1260f5455433c449ef272695786f04c2cefd93
ChaeRin-Im <ewuuu1112@gmail.com>
2024-04-03 12:18:22 +0900

    Style: 스웨거 설명 수정

--- src/main/java/com/tobe/healthy/workout/presentation/ExerciseController.java
+++ src/main/java/com/tobe/healthy/workout/presentation/ExerciseController.java
@@ -20,7 +20,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/exercise/v1")
-@Tag(name = "Exercise", description = "운동종류 API")
+@Tag(name = "06-00. 운동종류 API", description = "운동종류 API")
 @Slf4j
 public class ExerciseController {
 
