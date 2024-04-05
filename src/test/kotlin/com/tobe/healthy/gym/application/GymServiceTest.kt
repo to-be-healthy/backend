04321d1e86c0eac8f9c44ab71ebcd8ffc9fd3046
laborlawseon@gmail.com <laborlawseon@gmail.com>
2024-04-05 22:36:38 +0900

    test: "헬스장 테스트 코드 추가"

--- src/test/kotlin/com/tobe/healthy/gym/application/GymServiceTest.kt
+++ src/test/kotlin/com/tobe/healthy/gym/application/GymServiceTest.kt
@@ -2,17 +2,19 @@ package com.tobe.healthy.gym.application
 
 import com.querydsl.jpa.impl.JPAQueryFactory
 import com.tobe.healthy.config.error.CustomException
-import com.tobe.healthy.gym.domain.entity.Gym
+import com.tobe.healthy.config.error.ErrorCode.JOIN_CODE_NOT_VALID
+import com.tobe.healthy.gym.repository.GymRepository
+import com.tobe.healthy.log
 import com.tobe.healthy.member.application.MemberService
 import com.tobe.healthy.member.domain.entity.Member
 import com.tobe.healthy.member.domain.entity.MemberType.STUDENT
 import com.tobe.healthy.member.domain.entity.MemberType.TRAINER
 import com.tobe.healthy.member.repository.MemberRepository
 import jakarta.persistence.EntityManager
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.security.crypto.password.PasswordEncoder
@@ -26,19 +28,29 @@ class KotlinGymServiceTest @Autowired constructor(
     private val memberService: MemberService,
     private val gymService: GymService,
     private val passwordEncoder: PasswordEncoder,
-    private val em: EntityManager
+    private val em: EntityManager,
+    private val gymRepository: GymRepository
 ) {
 
     @BeforeEach
     fun `각 테스트마다 임시 회원을 생성한다`() {
-        val entity = Member.builder()
+        val student = Member.builder()
             .userId("test1234")
             .email("test1234@gmail.com")
             .password(passwordEncoder.encode("zxcvbnm11"))
             .name("test1234")
             .memberType(STUDENT)
             .build()
-        memberRepository.save(entity)
+        memberRepository.save(student)
+
+        val trainer = Member.builder()
+            .userId("test12345")
+            .email("test12345@gmail.com")
+            .password(passwordEncoder.encode("zxcvbnm11"))
+            .name("test12345")
+            .memberType(TRAINER)
+            .build()
+        memberRepository.save(trainer)
     }
 
     @Test
@@ -48,5 +60,31 @@ class KotlinGymServiceTest @Autowired constructor(
         em.flush()
         em.clear()
         assertThat(findMember.gym.id).isEqualTo(10)
+    }
+
+    @Test
+    fun `트레이너가 인증코드 없이 내 헬스장으로 등록해 실패한다`() {
+        val findMember = memberRepository.findByUserId("test12345").orElseThrow()
+
+        val message = assertThrows<CustomException> {
+            gymService.selectMyGym(10L, 0, findMember.id)
+        }.message
+
+        assertThat(message).isEqualTo(JOIN_CODE_NOT_VALID.message)
+    }
+
+    @Test
+    fun `관리자 또는 트레이너가 헬스장을 등록한다`() {
+        gymService.registerGym("선우짐 서초점")
+        val findGym = gymRepository.findByName("선우짐 서초점").orElseThrow()
+        assertThat(findGym.name).isEqualTo("선우짐 서초점")
+    }
+
+    @Test
+    fun `헬스장에 속한 트레이너들을 조회한다`() {
+        val lists = gymService.findAllTrainersByGym(1)
+        for (list in lists) {
+            log.info { "list => ${list}" }
+        }
     }
 }
--- src/test/kotlin/com/tobe/healthy/schedule/application/KotlinScheduleServiceTest.kt
+++ src/test/kotlin/com/tobe/healthy/schedule/application/KotlinScheduleServiceTest.kt
@@ -1,14 +1,14 @@
 package com.tobe.healthy.schedule.application
 
-import com.tobe.healthy.log
 import com.tobe.healthy.schedule.domain.dto.`in`.AutoCreateScheduleCommand
 import com.tobe.healthy.schedule.repository.ScheduleRepository
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDate
-import java.time.LocalTime
+import java.time.LocalTime.of
 
 @SpringBootTest
 @Transactional
@@ -22,17 +22,21 @@ class KotlinScheduleServiceTest @Autowired constructor(
         val request: AutoCreateScheduleCommand = AutoCreateScheduleCommand.builder()
             .startDt(LocalDate.of(2024, 4, 1))
             .endDt(LocalDate.of(2024, 4, 7))
-            .weekdayStartTime(LocalTime.of(10, 0))
-            .weekdayEndTime(LocalTime.of(20, 0))
-            .weekendStartTime(LocalTime.of(12, 0))
-            .weekendEndTime(LocalTime.of(18, 0))
+            .weekdayStartTime(of(10, 0))
+            .weekdayEndTime(of(20, 0))
+            .weekendStartTime(of(12, 0))
+            .weekendEndTime(of(18, 0))
             .lessonTime(50)
             .breakTime(10)
             .build()
 
         val results = scheduleService.autoCreateSchedule(request)
 
-        log.info { results }
+        assertThat(results.size).isEqualTo(7)
+    }
+
+    @Test
+    fun `일정을 등록한다`() {
 
     }
 }
