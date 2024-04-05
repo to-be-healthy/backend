9d566f6ec5d4cef62411eb17b43c07476b1f9f4e
jungseonw00 <laborlawseon@gmail.com>
2024-04-05 14:14:58 +0900

    fix: "kotlin 환경 삭제"

--- src/main/java/com/tobe/healthy/gym/application/GymService.java
+++ src/main/java/com/tobe/healthy/gym/application/GymService.java
@@ -7,16 +7,16 @@ import com.tobe.healthy.gym.domain.entity.Gym;
 import com.tobe.healthy.gym.repository.GymRepository;
 import com.tobe.healthy.member.domain.entity.Member;
 import com.tobe.healthy.member.repository.MemberRepository;
+import com.tobe.healthy.trainer.respository.TrainerMemberMappingRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.apache.commons.lang3.ObjectUtils;
-import org.modelmapper.ModelMapper;
 
 import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
+
 import static com.tobe.healthy.config.error.ErrorCode.*;
 import static com.tobe.healthy.member.domain.entity.MemberType.TRAINER;
 import static java.util.stream.Collectors.toList;
@@ -26,6 +26,7 @@ import static java.util.stream.Collectors.toList;
 @Transactional
 @Slf4j
 public class GymService {
+	private final TrainerMemberMappingRepository trainerMemberMappingRepository;
 
 	private final MemberRepository memberRepository;
 	private final GymRepository gymRepository;
@@ -73,17 +74,6 @@ public class GymService {
 		return memberRepository.findAllTrainerByGym(gymId).stream()
 			.map(TrainerCommandResult::new)
 			.toList();
-	}
-  
-	public Boolean selectMyTrainer(Long gymId, Long trainerId, Long memberId) {
-		TrainerMemberMapping entity = TrainerMemberMapping.create(gymId, trainerId, memberId);
-		trainerMemberMappingRepository.save(entity);
-		return true;
-	}
-
-	public List<MemberInTeamCommandResult> findAllMyMemberInTeam(Long memberId) {
-		List<Long> members = trainerMemberMappingRepository.findAllMembers(memberId).stream().map(m -> m.getMemberId()).collect(toList());
-		return memberRepository.findAll(members).stream().map(m -> new MemberInTeamCommandResult(m)).collect(Collectors.toList());
 	}
 
 	private int getJoinCode() {
