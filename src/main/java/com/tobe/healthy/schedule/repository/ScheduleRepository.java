fef3af4369f7f28ada4c39e84da6856774f4cb8d
ChaeRin-Im <ewuuu1112@gmail.com>
2024-04-27 14:17:23 +0900

    Fix: 학생이 수업 신청시 동시성 이슈 해결을 위한 Lock설정

--- src/main/java/com/tobe/healthy/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/tobe/healthy/schedule/repository/ScheduleRepository.java
@@ -1,7 +1,9 @@
 package com.tobe.healthy.schedule.repository;
 
 import com.tobe.healthy.schedule.domain.entity.Schedule;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
@@ -17,6 +19,7 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long>, Sched
 	@Query("select s from Schedule s left join fetch s.standBySchedule where s.id = :scheduleId and s.reservationStatus = 'COMPLETED' and s.applicant is not null and s.delYn = false")
 	Optional<Schedule> findAvailableStandById(Long scheduleId);
 
+	@Lock(value = LockModeType.PESSIMISTIC_WRITE)
 	@Query("select s from Schedule s where s.id = :scheduleId and s.reservationStatus = 'AVAILABLE' and s.applicant is null and s.delYn = false")
 	Optional<Schedule> findAvailableScheduleById(Long scheduleId);
 }
