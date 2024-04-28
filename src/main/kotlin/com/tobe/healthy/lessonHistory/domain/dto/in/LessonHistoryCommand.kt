e7bd4982625f6712afc9e87c6630940a4d51d7b9
seonwoo_jung <laborlawseon@gmail.com>
2024-04-28 18:11:41 +0900

    fix: "응답 DTO 메시지 수정"

--- src/main/kotlin/com/tobe/healthy/lessonHistory/domain/dto/in/LessonHistoryCommand.kt
+++ src/main/kotlin/com/tobe/healthy/lessonHistory/domain/dto/in/LessonHistoryCommand.kt
@@ -9,7 +9,7 @@ data class LessonHistoryCommand(
     @field:NotBlank(message = "수정할 수업일지 제목을 입력해 주세요.")
     val title: String?,
 
-    @field:NotBlank(message = "수정할 수업일지 내용")
-    @NotEmpty(message = "수정할 수업일지 내용을 입력해 주세요.")
+    @Schema(description = "수정할 수업일지 내용")
+    @field:NotBlank(message = "수정할 수업일지 내용을 입력해 주세요.")
     val content: String?
 )
