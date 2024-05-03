bef5110438266e4f19621eafb5e5c00a7d167a34
seonwoo_jung <laborlawseon@gmail.com>
2024-05-04 03:10:57 +0900

    fix: "kotest 작성중"

--- src/test/java/com/tobe/healthy/HealthyApplicationTests.java
+++ src/test/java/com/tobe/healthy/HealthyApplicationTests.java
@@ -1,23 +1,15 @@
 package com.tobe.healthy;
 
-import com.tobe.healthy.course.application.CourseService;
-import com.tobe.healthy.course.domain.dto.in.CourseUpdateCommand;
-import com.tobe.healthy.course.domain.entity.CourseHistoryType;
-import com.tobe.healthy.point.domain.entity.Calculation;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 @Slf4j
 class HealthyApplicationTests {
 
-	@Test
-	void contextLoads() {
-		boolean result = StringUtils.hasText("        ");
-		log.info("result => {}", result);
-	}
+    @Test
+    void contextLoads() {
+        boolean result = StringUtils.hasText("        ");
+        log.info("result => {}", result);
+    }
 }
