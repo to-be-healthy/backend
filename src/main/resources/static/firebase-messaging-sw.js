6971426ad2e2df8a8ab61b4e30dce01247b61bb9
seonwoo_jung <laborlawseon@gmail.com>
2024-05-07 21:00:27 +0900

    fix: "푸쉬 js 수정"

--- src/main/resources/static/firebase-messaging-sw.js
+++ src/main/resources/static/firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts('https://www.gstatic.com/firebasejs/5.9.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/5.9.2/firebase-messaging.js');
 
 // Initialize Firebase
-let firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyBaG-SAt2hzgha2C16CNZQGnnB0qdiH7_I",
     authDomain: "to-be-healthy-417411.firebaseapp.com",
     projectId: "to-be-healthy-417411",
@@ -11,5 +11,15 @@ let firebaseConfig = {
     appId: "1:827137813758:web:a36ff6e25eb30b32eb89af",
     measurementId: "G-68SZ9TBE6Y"
 };
+
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
+// 백그라운드 메시지 핸들링
+messaging.setBackgroundMessageHandler(function(payload) {
+    const title = 'Hello, World!';
+    const options = {
+        body: payload.data.status
+    };
+
+    return self.registration.showNotification(title, options);
+});
