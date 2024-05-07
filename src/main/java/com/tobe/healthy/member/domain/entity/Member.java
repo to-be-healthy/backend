7fd9b38769af1b74f7c7264c4e63db9a6b2f519f
seonwoo_jung <laborlawseon@gmail.com>
2024-05-07 15:12:25 +0900

    feat: "푸시 전송 API 추가"

--- src/main/java/com/tobe/healthy/member/domain/entity/Member.java
+++ src/main/java/com/tobe/healthy/member/domain/entity/Member.java
@@ -1,23 +1,5 @@
 package com.tobe.healthy.member.domain.entity;
 
-import com.tobe.healthy.common.BaseTimeEntity;
-import com.tobe.healthy.gym.domain.entity.Gym;
-import com.tobe.healthy.member.domain.dto.in.MemberJoinCommand;
-import com.tobe.healthy.schedule.domain.entity.Schedule;
-import com.tobe.healthy.schedule.domain.entity.ScheduleWaiting;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.ColumnDefault;
-import org.hibernate.annotations.DynamicUpdate;
-
-import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
 import static com.tobe.healthy.member.domain.entity.AlarmStatus.ENABLED;
 import static com.tobe.healthy.member.domain.entity.MemberType.STUDENT;
 import static com.tobe.healthy.member.domain.entity.SocialType.NONE;
@@ -27,6 +9,31 @@ import static jakarta.persistence.EnumType.STRING;
 import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
+
+import com.tobe.healthy.common.BaseTimeEntity;
+import com.tobe.healthy.gym.domain.entity.Gym;
+import com.tobe.healthy.member.domain.dto.in.MemberJoinCommand;
+import com.tobe.healthy.schedule.domain.entity.Schedule;
+import com.tobe.healthy.schedule.domain.entity.ScheduleWaiting;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import javax.annotation.Nullable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicUpdate;
 
 @Entity
 @NoArgsConstructor(access = PROTECTED)
@@ -94,8 +101,6 @@ public class Member extends BaseTimeEntity<Member, Long> {
 	private SocialType socialType = NONE;
 
 	private String nickname;
-
-	private String fcmToken;
 
 	@ColumnDefault("false")
 	private boolean delYn = false;
@@ -169,10 +174,6 @@ public class Member extends BaseTimeEntity<Member, Long> {
 
 	public void assignNickname(String nickname) {
 		this.nickname = nickname;
-	}
-
-	public void registerFcmToken(String fcmToken) {
-		this.fcmToken = fcmToken;
 	}
 
 	public void setMemberProfile(MemberProfile profile) {
--- src/main/kotlin/com/tobe/healthy/config/FirebaseConfig.kt
+++ src/main/kotlin/com/tobe/healthy/config/FirebaseConfig.kt
@@ -5,27 +5,24 @@ import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.tobe.healthy.log
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.stereotype.Service
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import java.io.FileInputStream
-import javax.annotation.PostConstruct
 
-@Service
+@Configuration
 class FirebaseConfig(
     @Value("\${firebase.admin-sdk.file}")
     private val firebaseAdminsdkFile: String
 ) {
 
-    @PostConstruct
-    fun initializeFCM() {
+    @Bean
+    fun initFirebase(): FirebaseApp {
         FileInputStream(firebaseAdminsdkFile).use {
             val options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(it))
                 .build()
-            log.info { "options => ${options}" }
-            if (FirebaseApp.getApps().isEmpty()) {
-                FirebaseApp.initializeApp(options)
-                log.info { "Firebase application has been initialized"}
-            }
+            log.info { "Firebase application has been initialized"}
+            return FirebaseApp.initializeApp(options)
         }
     }
 }
