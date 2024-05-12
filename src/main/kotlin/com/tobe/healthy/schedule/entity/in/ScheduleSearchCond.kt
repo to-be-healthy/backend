ef4691eb4370e7a8aa7b05a85c8d05199b60f31b
seonwoo_jung <laborlawseon@gmail.com>
2024-05-12 10:26:11 +0900

    feat: "조회조건이 전부 null일경우 현재 월을 기준으로 조회하도록 수정"

--- src/main/kotlin/com/tobe/healthy/schedule/entity/in/ScheduleSearchCond.kt
+++ src/main/kotlin/com/tobe/healthy/schedule/entity/in/ScheduleSearchCond.kt
@@ -2,14 +2,21 @@ package com.tobe.healthy.schedule.entity.`in`
 
 import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 data class ScheduleSearchCond(
     @Schema(description = "조회할 수업 일자", example = "2024-04")
-    val lessonDt: String? = null,
+    var lessonDt: String? = null,
 
     @Schema(description = "조회할 수업 시작 일자", example = "2024-04-01")
     val lessonStartDt: LocalDate? = null,
 
     @Schema(description = "조회할 수업 종료 일자", example = "2024-04-30")
     val lessonEndDt: LocalDate? = null
-)
+) {
+    init {
+        if (lessonDt == null && lessonStartDt == null && lessonEndDt == null) {
+            lessonDt = DateTimeFormatter.ofPattern("yyyy-MM").format(LocalDate.now()).toString()
+        }
+    }
+}
