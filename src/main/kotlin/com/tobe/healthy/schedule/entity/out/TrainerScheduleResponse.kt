e5a7b54cbe2272246a94bfa7a9a4dc5a2e075005
seonwoo_jung <laborlawseon@gmail.com>
2024-05-14 17:39:56 +0900

    fix: "기본 수업 시간 API에서 lessonTime 응답 방식 변경"

--- src/main/kotlin/com/tobe/healthy/schedule/entity/out/TrainerScheduleResponse.kt
+++ src/main/kotlin/com/tobe/healthy/schedule/entity/out/TrainerScheduleResponse.kt
@@ -1,6 +1,5 @@
 package com.tobe.healthy.schedule.entity.out
 
-import com.tobe.healthy.schedule.domain.entity.LessonTime
 import com.tobe.healthy.schedule.entity.TrainerScheduleClosedDaysInfo
 import com.tobe.healthy.schedule.entity.TrainerScheduleInfo
 import java.time.DayOfWeek
@@ -11,7 +10,7 @@ data class TrainerScheduleResponse(
     val lessonEndTime: LocalTime,
     val lunchStartTime: LocalTime? = null,
     val lunchEndTime: LocalTime? = null,
-    val lessonTime: LessonTime,
+    val lessonTime: Int,
     val trainerScheduleClosedDays: List<TrainerScheduleClosedDaysResponse> = mutableListOf()
 ) {
     companion object {
@@ -21,7 +20,7 @@ data class TrainerScheduleResponse(
                 lessonEndTime = trainerScheduleInfo.lessonEndTime,
                 lunchStartTime = trainerScheduleInfo.lunchStartTime,
                 lunchEndTime = trainerScheduleInfo.lunchEndTime,
-                lessonTime = trainerScheduleInfo.lessonTime,
+                lessonTime = trainerScheduleInfo.lessonTime.description,
                 trainerScheduleClosedDays = trainerScheduleInfo.trainerScheduleClosedDays?.map {
                     TrainerScheduleClosedDaysResponse.from(it)
                 }?.toList() ?: mutableListOf()
