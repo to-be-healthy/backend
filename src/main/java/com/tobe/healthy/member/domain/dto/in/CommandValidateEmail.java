71cd3bd031d8c7ad28298164c077a3034433ac32
seonwoo_jung <laborlawseon@gmail.com>
2024-05-28 10:12:24 +0900

    fix: "DTO 기본 생성자 추가"

--- src/main/java/com/tobe/healthy/member/domain/dto/in/CommandValidateEmail.java
+++ src/main/java/com/tobe/healthy/member/domain/dto/in/CommandValidateEmail.java
@@ -4,9 +4,11 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CommandValidateEmail {
     @NotEmpty(message = "이메일을 입력해 주세요.")
     @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$", message = "올바른 이메일 형식을 입력해 주세요.")
