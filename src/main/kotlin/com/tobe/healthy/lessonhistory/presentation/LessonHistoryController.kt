c26cafb6815dbfde0a53593dca78fb04c920bac4
seonwoo_jung <laborlawseon@gmail.com>
2024-05-29 13:55:34 +0900

    fix: "수업일지 API 변경"

--- src/main/kotlin/com/tobe/healthy/lessonhistory/presentation/LessonHistoryController.kt
+++ src/main/kotlin/com/tobe/healthy/lessonhistory/presentation/LessonHistoryController.kt
@@ -17,7 +17,10 @@ import org.springdoc.core.annotations.ParameterObject
 import org.springframework.data.domain.Pageable
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/lessonhistory/v1")
@@ -113,7 +116,7 @@ class LessonHistoryController(
         ])
     @GetMapping("/unwritten")
     fun findAllUnwrittenLessonHistory(
-        @RequestBody request: UnwrittenLessonHistorySearchCond,
+        request: UnwrittenLessonHistorySearchCond,
         @AuthenticationPrincipal member: CustomMemberDetails
     ): ApiResultResponse<List<RetrieveUnwrittenLessonHistory>> {
         return ApiResultResponse(
