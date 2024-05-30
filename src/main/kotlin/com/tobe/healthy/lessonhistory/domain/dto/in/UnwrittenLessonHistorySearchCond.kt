2d3af9dcba1db4bdd3d868ed85c47923a9051426
seonwoo_jung <laborlawseon@gmail.com>
2024-05-30 18:12:25 +0900

    fix: "수업일지 lessonDateTime 파라미터 없을경우 기본값"

--- src/main/kotlin/com/tobe/healthy/lessonhistory/domain/dto/in/UnwrittenLessonHistorySearchCond.kt
+++ src/main/kotlin/com/tobe/healthy/lessonhistory/domain/dto/in/UnwrittenLessonHistorySearchCond.kt
@@ -1,6 +1,17 @@
 package com.tobe.healthy.lessonhistory.domain.dto.`in`
 
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale.KOREAN
+
 data class UnwrittenLessonHistorySearchCond(
-    val lessonDateTime: String? = null,
+    val lessonDateTime: String? = formatDate(LocalDateTime.now()),
     val studentId: Long? = null
-)
+) {
+    companion object {
+        private fun formatDate(lessonDateTime: LocalDateTime): String {
+            val formatter = DateTimeFormatter.ofPattern("YYYY-MM-DD", KOREAN)
+            return lessonDateTime.format(formatter)
+        }
+    }
+}
