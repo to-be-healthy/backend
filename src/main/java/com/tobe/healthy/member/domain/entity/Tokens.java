7583c1240440dc6d6d9fadacf7b7ab7f173f4d86
seonwoo_jung <laborlawseon@gmail.com>
2024-06-03 13:01:39 +0900

    fix: "로그인시 회원 이름도 응답에 추가"

--- src/main/java/com/tobe/healthy/member/domain/entity/Tokens.java
+++ src/main/java/com/tobe/healthy/member/domain/entity/Tokens.java
@@ -8,14 +8,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Tokens {
 	private Long memberId;
+	private String name;
     private String accessToken;
     private String refreshToken;
     private String userId;
     private MemberType memberType;
 	private Long gymId;
 
-	public Tokens(Long memberId, String accessToken, String refreshToken, String userId, MemberType memberType, Gym gym) {
+	public Tokens(Long memberId, String name, String accessToken, String refreshToken, String userId, MemberType memberType, Gym gym) {
 		this.memberId = memberId;
+		this.name = name;
 		this.accessToken = accessToken;
 		this.refreshToken = refreshToken;
 		this.userId = userId;
