585d5b0a0fa427cd36505f526c82789dfeae4d62
seonwoo_jung <laborlawseon@gmail.com>
2024-06-14 15:26:28 +0900

    fix: "수업일지 조회 프로퍼티 추가"

--- src/main/kotlin/com/tobe/healthy/lessonhistory/domain/dto/out/CustomRetrieveLessonHistoryByDateCondResult.kt
+++ src/main/kotlin/com/tobe/healthy/lessonhistory/domain/dto/out/CustomRetrieveLessonHistoryByDateCondResult.kt
@@ -1,11 +1,15 @@
 package com.tobe.healthy.lessonhistory.domain.dto.out
 
 data class CustomRetrieveLessonHistoryByDateCondResult(
+    val studentName: String?,
     val content: List<RetrieveLessonHistoryByDateCondResult?>
 ) {
     companion object {
         fun from(entity: List<RetrieveLessonHistoryByDateCondResult>): CustomRetrieveLessonHistoryByDateCondResult {
-            return CustomRetrieveLessonHistoryByDateCondResult(content = entity)
+            return CustomRetrieveLessonHistoryByDateCondResult(
+                studentName = entity.firstOrNull()?.student,
+                content = entity
+            )
         }
     }
 }
