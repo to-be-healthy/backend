d83cd9b0853d2ba99145ef1f8b33d12b08410c9e
ChaeRin-Im <ewuuu1112@gmail.com>
2024-06-14 14:26:32 +0900

    Fix: 학생홈 가장 최근 수업일지 보여주기, 트레이너 프로필 사진 추가

--- src/main/java/com/tobe/healthy/schedule/domain/dto/out/MyReservation.java
+++ src/main/java/com/tobe/healthy/schedule/domain/dto/out/MyReservation.java
@@ -4,6 +4,7 @@ import com.tobe.healthy.schedule.domain.entity.Schedule;
 import lombok.Builder;
 import lombok.Data;
 import lombok.ToString;
+import org.springframework.util.ObjectUtils;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -17,15 +18,19 @@ public class MyReservation {
     private LocalTime lessonStartTime;
     private LocalTime lessonEndTime;
     private String trainerName;
+    private String trainerProfile;
     private String reservationStatus;
 
     public static MyReservation from(Schedule schedule) {
-        return MyReservation.builder().scheduleId(schedule.getId())
+        MyReservationBuilder builder = MyReservation.builder().scheduleId(schedule.getId())
                 .lessonDt(schedule.getLessonDt())
                 .lessonStartTime(schedule.getLessonStartTime())
                 .lessonEndTime(schedule.getLessonEndTime())
                 .trainerName(schedule.getTrainer().getName() + " 트레이너")
-                .reservationStatus(schedule.getReservationStatus().name())
-                .build();
+                .reservationStatus(schedule.getReservationStatus().name());
+        if(!ObjectUtils.isEmpty(schedule.getTrainer().getMemberProfile())){
+            builder.trainerProfile(schedule.getTrainer().getMemberProfile().getFileUrl());
+        }
+        return builder.build();
     }
 }
