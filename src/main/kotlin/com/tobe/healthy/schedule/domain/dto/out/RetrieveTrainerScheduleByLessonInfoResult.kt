90f15f80a8ee60b1ef3be6847ef51fa397dab628
seonwoo_jung <laborlawseon@gmail.com>
2024-07-19 16:34:36 +0900

    fix: "스케줄 조회시 해당 주의 가장 빠른 수업 시작 시간, 가장 늦은 수업 종료 시간 구현"

--- src/main/kotlin/com/tobe/healthy/schedule/domain/dto/out/RetrieveTrainerScheduleByLessonInfoResult.kt
+++ src/main/kotlin/com/tobe/healthy/schedule/domain/dto/out/RetrieveTrainerScheduleByLessonInfoResult.kt
@@ -2,7 +2,9 @@ package com.tobe.healthy.schedule.domain.dto.out
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import com.tobe.healthy.common.LessonDetailResultSerializer
+import com.tobe.healthy.common.LessonTimeFormatter.formatLessonTime
 import com.tobe.healthy.schedule.domain.entity.ReservationStatus
+import com.tobe.healthy.schedule.domain.entity.ReservationStatus.DISABLED
 import com.tobe.healthy.schedule.domain.entity.Schedule
 import java.time.Duration
 import java.time.LocalDate
@@ -10,38 +12,44 @@ import java.time.LocalTime
 
 data class RetrieveTrainerScheduleByLessonInfoResult(
     val trainerName: String?,
-    val schedule: Map<LocalDate, List<LessonDetailResult>>?
+    val earliestLessonStartTime: String?,
+    val latestLessonEndTime: String?,
+    val schedule: Map<LocalDate, List<LessonDetailResult>>?,
 ) {
     companion object {
         private const val DEFAULT_DURATION = 60.0
         fun from(schedule: List<Schedule>): RetrieveTrainerScheduleByLessonInfoResult {
-            return schedule.let {
-                val groupingSchedules = schedule.groupBy { it.lessonDt }
-                    .mapValues { entry ->
-                        entry.value.map { schedule ->
-                            LessonDetailResult(
-                                scheduleId = schedule.id,
-                                duration = calculateDuration(schedule),
-                                lessonStartTime = schedule.lessonStartTime,
-                                lessonEndTime = schedule.lessonEndTime,
-                                reservationStatus = schedule.reservationStatus,
-                                applicantId = schedule.applicant?.id,
-                                applicantName = schedule.applicant?.name,
-                                waitingStudentId = schedule.scheduleWaiting?.firstOrNull()?.member?.id,
-                                waitingStudentName = schedule.scheduleWaiting?.firstOrNull()?.member?.name
-                            )
-                        }
-                    }
+            val firstLessonStartTime = schedule.filter { it.reservationStatus != DISABLED }.minOfOrNull { it.lessonStartTime }
+            val lastLessonEndTime = schedule.filter { it.reservationStatus != DISABLED }.maxOfOrNull { it.lessonEndTime }
 
-                RetrieveTrainerScheduleByLessonInfoResult(
-                    trainerName = schedule.firstOrNull()?.trainer?.name?.let { "${it} 트레이너" },
-                    schedule = groupingSchedules.ifEmpty { null }
-                )
-            }
+            val groupingSchedules = schedule.groupBy { it.lessonDt }
+                .mapValues { entry ->
+                    entry.value.map { schedule ->
+                        LessonDetailResult(
+                            scheduleId = schedule.id,
+                            duration = calculateDuration(schedule),
+                            lessonStartTime = schedule.lessonStartTime,
+                            lessonEndTime = schedule.lessonEndTime,
+                            reservationStatus = schedule.reservationStatus,
+                            applicantId = schedule.applicant?.id,
+                            applicantName = schedule.applicant?.name,
+                            waitingStudentId = schedule.scheduleWaiting?.firstOrNull()?.member?.id,
+                            waitingStudentName = schedule.scheduleWaiting?.firstOrNull()?.member?.name
+                        )
+                    }
+                }
+
+            return RetrieveTrainerScheduleByLessonInfoResult(
+                trainerName = schedule.firstOrNull()?.trainer?.name?.let { "${it} 트레이너" },
+                schedule = groupingSchedules.ifEmpty { null },
+                earliestLessonStartTime = formatLessonTime(firstLessonStartTime),
+                latestLessonEndTime = formatLessonTime(lastLessonEndTime)
+            )
         }
 
         private fun calculateDuration(schedule: Schedule): Double {
-            val duration = Duration.between(schedule.lessonStartTime, schedule.lessonEndTime).toMinutes() / DEFAULT_DURATION
+            val duration = Duration.between(schedule.lessonStartTime, schedule.lessonEndTime)
+                .toMinutes() / DEFAULT_DURATION
             return if (duration < 0) {
                 duration.plus(24)
             } else {
